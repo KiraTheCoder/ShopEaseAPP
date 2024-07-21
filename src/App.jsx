fc0e811230b1f6a18bf292c955427ee2a0ef6757
kiran <kiranbohraofficial@gmail.com>
2024-07-21 15:44:44 +0530

    sucessfuly set api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import TestAPP from "@/__TEST__/TestAPP"
+import { getData } from "@/services/apiCall/index"
+import { useState,useEffect } from "react"
+
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getData('/sample')
+      .then((response) => setData(response))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  console.log(data);
+
   return (
     <>
-    <TestAPP/>
+    {/* <TestAPP/> */}
+    <h1>{data?.message}</h1>
     </>
   )
 }
