e4849e09a9c10a96262fbf01aaf0effced3f84ad
kiran <kiranbohraofficial@gmail.com>
2024-07-24 10:16:09 +0530

    add image

--- src/layouts/Footer/footersection/logo.jsx
+++ src/layouts/Footer/footersection/logo.jsx
@@ -1,7 +1,10 @@
+import logoImage from "@/assets/images/footerImages/googlepay.png"
 function Logo() {
     return(
         <>
-        <div className="text-[20px] sm:text-[22px] md:text-[24px] font-bold mb-3 text-[#fafafa]">Exclusive</div>
+        <div className="text-[20px] sm:text-[22px] md:text-[24px] font-bold mb-3 text-[#fafafa]">
+            <img src={logoImage} alt="logo" />
+            Exclusive</div>
         </>
     )
 }
