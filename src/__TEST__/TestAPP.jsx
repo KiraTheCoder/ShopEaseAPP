85466e2bb850b76bc6a7cb3675ccf29be2198cf4
KHUSHI <tamorkhushi@gmail.com>
2024-07-29 12:11:09 +0530

    khushi

--- src/__TEST__/TestAPP.jsx
+++ src/__TEST__/TestAPP.jsx
@@ -1,18 +1,22 @@
 import Header from "@/Header/Header"
 import Store from "./Store"
 import  Footer from '../layouts/Footer/index'
-import LogInPage from  '../pages/LoginPage/LogInPage'
+// import LogInPage from  '../pages/LoginPage/LogInPage'
 // import Index from "../pages/SignUpPage/index"
-import SignupPage from "../pages/SignUpPage/SignupPage"
+// import SignupPage from "../pages/SignUpPage/SignupPage"
 const TestAPP = () => {
   return (
     <div>
 {/* <<<<<<< HEAD */}
           <Header/>
 {/* ======= */}
+
           {/* <Store /> */}
-          <LogInPage/>
-          <SignupPage/>
+          <
+          {/* <LogInPage/> */}
+          {/* <SignupPage/> */}
+          
+
           <Footer/>
 {/* >>>>>>> e4849e09a9c10a96262fbf01aaf0effced3f84ad */}
     </div>
