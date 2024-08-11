aa67ef29db635bd1897c67aa2920a9b9f8ab550d
kiran <kiranbohraofficial@gmail.com>
2024-08-11 14:53:35 +0530

    fix imges

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,11 +1,11 @@
 export const ProductCard = ({ products }) => {
-  // console.log(products.image[0]);
-  console.log(products);
   return (
     <div className="w-[15rem] bg-slate-300 rounded overflow-hidden shadow-lg p-4 m-4">
-      {products?.images?.map((image, index) => (
-        <img key={index} className="w-full h-[8rem]" src={`data:${image.contentType};base64,${image.data};`} alt={products?.productName} />
-      ))}
+      {products[0].images?.map((image, index) => 
+     {   console.log(image);
+      return <img key={index} className="w-full h-[8rem]" src={`data:${image.contentType};base64,${image.data}`} alt={products?.productName} />
+    }
+      )}
       {/* <div className="px-6 py-4">
         <h2 className="text-xl font-bold">{products.productName}</h2>
         <p>{products.description}</p>
